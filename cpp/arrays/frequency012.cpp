4f51ef8510c5b4f9eabf8b9205b43f77301f29fe
AnjaliG1999 <anjali24.7goyal@gmail.com>
2020-12-10 21:53:39 +0530

    Implementing Dutch National Flag Algorithm
    A simpler and more efficient way to sort an array only containing 0s, 1s, and 2s.

--- cpp/arrays/frequency012.cpp
+++ cpp/arrays/frequency012.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+}
+
+int swap(int *a, int *b){
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+
 int frequency1(int arr[], int n){
-    int sortedArr[n];
+    // int sortedArr[n];
     int count0 = 0, count1 = 0, count2 = 0;
     for(int i=0; i<n; i++){
         if(arr[i]==0)
@@ -14,19 +25,39 @@ int frequency1(int arr[], int n){
     }
 
     for(int i=0; i<count0; i++)
-        sortedArr[i] = 0;
+        arr[i] = 0;
     for(int i=count0; i<count0+count1; i++)
-        sortedArr[i] =1;
+        arr[i] =1;
     for(int i=count0+count1; i<n; i++)
-        sortedArr[i] = 2;
+        arr[i] = 2;
 
-    for(int i=0; i<n; i++)
-        cout << sortedArr[i] << " ";
+    printArray(arr, n);
+}
+
+int dutchNationalFlagAlgo(int arr[], int n) {
+    int low = 0, mid = 0, high = n-1;
+    while(mid <= high) {
+        if(arr[mid]==0){
+            swap(arr+mid, arr+low);
+            low++;
+            mid++;
+        } else if(arr[mid]==1){
+            mid++;
+        } else {
+            swap(arr+mid, arr+high);
+            high--;
+        }
+    }
+
+    printArray(arr, n);
 }
 
 int main(){
     int arr[] = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
     int n = sizeof(arr)/ sizeof(arr[0]);
-//    cout << frequency1(arr, n);
-    frequency1(arr, n);
+    cout << "Array: ";
+    printArray(arr, n);
+    cout << "\nSorted array: ";
+    // frequency1(arr, n);
+    dutchNationalFlagAlgo(arr, n);
 }
