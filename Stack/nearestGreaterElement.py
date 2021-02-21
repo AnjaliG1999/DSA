f66f111fb39bd2867dd8c009b7222bf4401553ea
AnjaliG1999 <anjali24.7goyal@gmail.com>
2021-02-21 20:00:50 +0530

    Given an array, print the Next Greater Element (NGE) for every element.
    The Next greater Element for an element x is the first greater element on the right/ left side of x in the array. Elements for which no greater element exist, consider next greater element as -1.

---
+++ Stack/nearestGreaterElement.py
@@ -0,0 +1,38 @@
+def nearestGreaterElementRight(arr):
+    stack = []
+    output = [None] * n
+
+    for i in range(n-1, -1, -1):
+        while (len(stack)!=0) and (arr[i] > stack[-1]):
+            stack.pop()
+        if len(stack)==0:
+            output[i] = -1
+        else:
+            output[i] = stack[-1]
+        stack.append(arr[i])
+    
+    return output
+
+def nearestGreaterElementLeft(arr):
+    stack = []
+    output = [None] * n
+
+    for i in range(n):
+        while (len(stack)!=0) and (arr[i] > stack[-1]):
+            stack.pop()
+        if len(stack)==0:
+            output[i] = -1
+        else:
+            output[i] = stack[-1]
+        stack.append(arr[i])
+    
+    return output
+
+arr = [10, 5, 11, 6, 20, 12]
+n = len(arr)
+
+ngeRight = nearestGreaterElementRight(arr)
+ngeLeft = nearestGreaterElementLeft(arr)
+
+print("The next greatest elements of {} to right are {}".format(arr, ngeRight))
+print("The next greatest elements of {} to left are {}".format(arr, ngeLeft))
---
+++ cpp/stack/nearestGreaterElement.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++)
+        cout << arr[i] << " ";
+}
+
+int nearestGreaterElementRight (int arr[], int n) {
+    int output[n];
+    stack<int> s;
+
+    for(int i=n-1; i>=0; i--) {
+        while(s.empty() == false && arr[i] > s.top())
+            s.pop();
+        if (s.empty())
+            output[i] = -1;
+        else
+            output[i] = s.top();
+
+        s.push(arr[i]);
+    }
+    printArray(output, n);
+}
+
+int nearestGreaterElementLeft (int arr[], int n) {
+    int output[n];
+    stack<int> s;
+
+    for(int i=0; i<n; i++) {
+        while(s.empty() == false && arr[i] > s.top())
+            s.pop();
+        if (s.empty())
+            output[i] = -1;
+        else
+            output[i] = s.top();
+
+        s.push(arr[i]);
+    }
+    printArray(output, n);
+}
+
+int main(){
+    int arr[] = {10, 5, 11, 6, 20, 12};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "The next greatest elements of ";
+    printArray(arr, n);
+    cout << "to right are ";
+    nearestGreaterElementRight(arr, n);
+
+    cout << "\nThe next greatest elements of ";
+    printArray(arr, n);
+    cout << "to left are ";
+    nearestGreaterElementLeft(arr, n);
+}
